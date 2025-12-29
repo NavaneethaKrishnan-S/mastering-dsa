8c42d48cfbf579ddcab2360ab92bac70b572abdf
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2025-12-29 16:20:41 +0530

    Implementing Graph

---
+++ src/main/java/com/masteringdsa/dsaconcepts/graph/Main.java
@@ -0,0 +1,19 @@
+package com.masteringdsa.dsaconcepts.graph;
+
+public class Main {
+    public static void main(String[] args) {
+        var graph = new Graph();
+
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addNode("D");
+
+        graph.addEdge("A", "B");
+        graph.addEdge("B", "D");
+        graph.addEdge("D", "C");
+        graph.addEdge("A", "C");
+
+        graph.traverseBreadthFirst("K");
+    }
+}
