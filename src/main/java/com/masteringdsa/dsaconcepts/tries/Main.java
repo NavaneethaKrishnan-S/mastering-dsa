e64ca1d7ab94dcd7a2809bdd2a6cf81bc3b80f4d
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2025-12-29 16:20:14 +0530

    Implementing Tries

---
+++ src/main/java/com/masteringdsa/dsaconcepts/tries/Main.java
@@ -0,0 +1,15 @@
+package com.masteringdsa.dsaconcepts.tries;
+
+public class Main {
+    public static void main(String[] args) {
+        var trie = new Trie();
+        trie.insert("car");
+        trie.insert("care");
+        trie.insert("card");
+        trie.insert("careful");
+        trie.insert("egg");
+
+        var words = trie.findWords("cargo");
+        System.out.println(words);
+    }
+}
